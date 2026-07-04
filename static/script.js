18da7352df39ed8728c08a74c3d2e9250f9dfca9
ANKIT <sycocoders@gmail.com>
2026-07-04 14:15:54 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,32 @@
 
+const ctx = document.getElementById('growthChart');
+
+new Chart(ctx,{
+
+type:'line',
+
+data:{
+
+labels:["Jan","Feb","Mar","Apr","May","Jun"],
+
+datasets:[{
+
+label:"Growth",
+
+data:[12,25,18,35,42,55],
+
+borderWidth:3,
+
+fill:false
+
+}]
+
+},
+
+options:{
+
+responsive:true
+
+}
+
+});
