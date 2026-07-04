ec5da06db5d1c5510dbd3785ecddcce1e5b2c3c0
ANKIT <sycocoders@gmail.com>
2026-07-04 14:23:24 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -19,7 +19,40 @@ def search_channel(channel_name):
 
 
 def get_channel_data(channel_name):
+    
+def get_latest_videos(channel_id):
 
+    url = (
+        f"https://www.googleapis.com/youtube/v3/search"
+        f"?key={API_KEY}"
+        f"&channelId={channel_id}"
+        f"&part=snippet"
+        f"&order=date"
+        f"&maxResults=6"
+    )
+
+    response = requests.get(url).json()
+
+    videos = []
+
+    if "items" not in response:
+        return videos
+
+    for item in response["items"]:
+
+        if item["id"]["kind"] == "youtube#video":
+
+            videos.append({
+
+                "title": item["snippet"]["title"],
+
+                "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
+
+                "videoId": item["id"]["videoId"]
+
+            })
+
+    return videos
     channel_id = search_channel(channel_name)
 
     if channel_id is None:
