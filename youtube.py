8cec00a4a43173b581afcd9cb626e5ea37021225
ANKIT <sycocoders@gmail.com>
2026-07-04 14:53:08 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,6 +1,5 @@
 import requests
-
-API_KEY = "YOUR_API_KEY"
+from config import API_KEY
 
 
 def search_channel(channel_name):
@@ -19,7 +18,54 @@ def search_channel(channel_name):
 
 
 def get_channel_data(channel_name):
-    
+
+    channel_id = search_channel(channel_name)
+
+    if channel_id is None:
+        return None
+
+    url = (
+        f"https://www.googleapis.com/youtube/v3/channels"
+        f"?part=snippet,statistics"
+        f"&id={channel_id}"
+        f"&key={API_KEY}"
+    )
+
+    response = requests.get(url).json()
+
+    item = response["items"][0]
+
+    return {
+
+        "channelId": channel_id,
+
+        "name": item["snippet"]["title"],
+
+        "logo": item["snippet"]["thumbnails"]["high"]["url"],
+
+        "description": item["snippet"]["description"],
+
+        "country": item["snippet"].get("country", "Unknown"),
+
+        "published": item["snippet"]["publishedAt"][:10],
+
+        "subscribers": "{:,}".format(
+            int(item["statistics"].get("subscriberCount", 0))
+        ),
+
+        "views": "{:,}".format(
+            int(item["statistics"].get("viewCount", 0))
+        ),
+
+        "videos": "{:,}".format(
+            int(item["statistics"].get("videoCount", 0))
+        ),
+
+        "score": "95/100"
+
+    }
+
+
 def get_latest_videos(channel_id):
 
     url = (
@@ -53,45 +99,3 @@ def get_latest_videos(channel_id):
             })
 
     return videos
-    channel_id = search_channel(channel_name)
-
-    if channel_id is None:
-        return None
-
-    url = (
-        f"https://www.googleapis.com/youtube/v3/channels"
-        f"?part=snippet,statistics"
-        f"&id={channel_id}"
-        f"&key={API_KEY}"
-    )
-
-    response = requests.get(url).json()
-
-    item = response["items"][0]
-
-    return {
-
-        "name": item["snippet"]["title"],
-
-        "logo": item["snippet"]["thumbnails"]["high"]["url"],
-
-        "description": item["snippet"]["description"],
-
-        "country": item["snippet"].get("country", "Unknown"),
-
-        "published": item["snippet"]["publishedAt"][:10],
-
-        "subscribers": "{:,}".format(
-            int(item["statistics"].get("subscriberCount", 0))
-        ),
-
-        "views": "{:,}".format(
-            int(item["statistics"].get("viewCount", 0))
-        ),
-
-        "videos": "{:,}".format(
-            int(item["statistics"].get("videoCount", 0))
-        ),
-
-        "score": "95/100"
-    }
