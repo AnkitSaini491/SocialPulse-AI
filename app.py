9ff359aa6dd2efa6e0f7755e909a7d7d1b75d6e8
ANKIT <sycocoders@gmail.com>
2026-07-04 14:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from youtube import get_channel_data
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-@app.route("/analyze", methods=["POST"])
+@@app.route("/analyze", methods=["POST"])
 def analyze():
+
+    channel = request.form["channel"]
+
+    data = get_channel_data(channel)
+
+    if data is None:
+        return "Channel Not Found"
+
+    videos = get_latest_videos(data["channelId"])
+
+    return render_template(
+
+        "dashboard.html",
+
+        data=data,
+
+        videos=videos
+
+    )
 
     channel = request.form["channel"]
 
