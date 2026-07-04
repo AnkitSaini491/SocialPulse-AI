5ea80737ff816711bb0acc53ff121964604a5790
ANKIT <sycocoders@gmail.com>
2026-07-04 14:13:29 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,1 +1,13 @@
+import requests
 
+API_KEY = "YOUR_YOUTUBE_API_KEY"
+
+def get_channel_data(channel_name):
+
+    return {
+        "name": channel_name,
+        "subscribers": "Loading...",
+        "views": "Loading...",
+        "videos": "Loading...",
+        "score": "Loading..."
+    }
