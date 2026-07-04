18901ac36d7db5cb1d89baa1126d37a18909d129
ANKIT <sycocoders@gmail.com>
2026-07-04 14:46:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
-from youtube import get_channel_data
+from youtube import get_channel_data, get_latest_videos
 
 app = Flask(__name__)
 
-@@app.route("/analyze", methods=["POST"])
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/analyze", methods=["POST"])
 def analyze():
 
-    channel = request.form["channel"]
+    channel = request.form.get("channel")
 
     data = get_channel_data(channel)
 
@@ -16,23 +22,16 @@ def analyze():
     videos = get_latest_videos(data["channelId"])
 
     return render_template(
-
         "dashboard.html",
-
         data=data,
-
         videos=videos
-
     )
 
-    channel = request.form["channel"]
 
-    data = get_channel_data(channel)
+@app.route("/compare")
+def compare():
+    return render_template("compare.html")
 
-    if data is None:
-        return "Channel Not Found"
 
-    return render_template(
-        "dashboard.html",
-        data=data
-    )
+if __name__ == "__main__":
+    app.run(debug=True)
