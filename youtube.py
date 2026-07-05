034f62278329f1d3acd8e0f2759cadbf6d7d3d84
ANKIT <sycocoders@gmail.com>
2026-07-05 17:01:03 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,11 +1,11 @@
+
 import requests
 from config import API_KEY
 
 
 def search_channel(channel_name):
-
     url = (
-        f"https://www.googleapis.com/youtube/v3/search"
+        "https://www.googleapis.com/youtube/v3/search"
         f"?part=snippet&type=channel&q={channel_name}&key={API_KEY}"
     )
 
@@ -25,7 +25,7 @@ def get_channel_data(channel_name):
         return None
 
     url = (
-        f"https://www.googleapis.com/youtube/v3/channels"
+        "https://www.googleapis.com/youtube/v3/channels"
         f"?part=snippet,statistics"
         f"&id={channel_id}"
         f"&key={API_KEY}"
@@ -33,46 +33,39 @@ def get_channel_data(channel_name):
 
     response = requests.get(url).json()
 
+    if "items" not in response or len(response["items"]) == 0:
+        return None
+
     item = response["items"][0]
 
     return {
-
         "channelId": channel_id,
-
         "name": item["snippet"]["title"],
-
         "logo": item["snippet"]["thumbnails"]["high"]["url"],
-
         "description": item["snippet"]["description"],
-
         "country": item["snippet"].get("country", "Unknown"),
-
         "published": item["snippet"]["publishedAt"][:10],
-
         "subscribers": "{:,}".format(
             int(item["statistics"].get("subscriberCount", 0))
         ),
-
         "views": "{:,}".format(
             int(item["statistics"].get("viewCount", 0))
         ),
-
         "videos": "{:,}".format(
             int(item["statistics"].get("videoCount", 0))
         ),
-
         "score": "95/100"
-
     }
 
 
 def get_latest_videos(channel_id):
 
     url = (
-        f"https://www.googleapis.com/youtube/v3/search"
+        "https://www.googleapis.com/youtube/v3/search"
         f"?key={API_KEY}"
         f"&channelId={channel_id}"
         f"&part=snippet"
+        f"&type=video"
         f"&order=date"
         f"&maxResults=6"
     )
@@ -86,26 +79,23 @@ def get_latest_videos(channel_id):
 
     for item in response["items"]:
 
-        if item["id"]["kind"] == "youtube#video":
-
-            videos.append({
-
-                "title": item["snippet"]["title"],
-
-                "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
-
-                "videoId": item["id"]["videoId"]
-
-            })
+        videos.append({
+            "title": item["snippet"]["title"],
+            "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
+            "videoId": item["id"]["videoId"]
+        })
 
     return videos
-    def get_top_video(channel_id):
+
+
+def get_top_video(channel_id):
 
     url = (
-        f"https://www.googleapis.com/youtube/v3/search"
+        "https://www.googleapis.com/youtube/v3/search"
         f"?key={API_KEY}"
         f"&channelId={channel_id}"
         f"&part=snippet"
+        f"&type=video"
         f"&order=viewCount"
         f"&maxResults=1"
     )
@@ -118,11 +108,7 @@ def get_latest_videos(channel_id):
     item = response["items"][0]
 
     return {
-
         "title": item["snippet"]["title"],
-
         "thumbnail": item["snippet"]["thumbnails"]["high"]["url"],
-
         "videoId": item["id"]["videoId"]
-
     }
